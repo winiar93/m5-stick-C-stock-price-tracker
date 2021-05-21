10e2c920ceba5d42a7d8ae146cf55586b82e1736
Mike <winiar93@gmail.com>
2021-05-22 01:31:08 +0200

    add gold price

--- stock_tracker.py
+++ stock_tracker.py
@@ -1,22 +1,16 @@
 from m5stack import *
 from m5ui import *
 from uiflow import *
-
-setScreenColor(0x111111)
-
-setScreenColor(0x111111)
-
 import urequests as requests
+
+
 setScreenColor(0x111111)
 
 
-gold_price_api = "https://api.thingspeak.com/apps/thinghttp/send_request?api_key=8MR8VNV0VF4EPKTM"
+gold_price_api = "https://api.thingspeak.com/apps/thinghttp/send_request?api_key=AT8VTTC4BMVYUWRT"
 
-response =  requests.get(gold_price_api).text
-gold_data_list = response.split(" ")
-
-
-
-
-label0 = M5TextBox(111, 12, gold_data_list[0], lcd.FONT_DejaVu18, 0xFFFFFF, rotate=90)
-
+response = requests.get(gold_price_api).text
+gold_data = response.split(" ")[0]
+label0 = M5TextBox(127, 67, "Gold", lcd.FONT_DejaVu40, 0xffd11a, rotate=90)
+label1 = M5TextBox(75, 35, gold_data, lcd.FONT_DejaVu40, 0xFFFFFF, rotate=90)
+label2 = M5TextBox(25, 75, "usd/oz", lcd.FONT_DejaVu24, 0xFFFFFF, rotate=90)
